8927bc9925211a8f73e839abba978f1c26e3beb9
Justin Babilino <jlbabilino@Justins-Air.verizon.net>
2022-06-27 12:38:40 -0400

    restructured for cmake

--- javaapi/src/main/java/org/team2363/helixtrajectory/HelixTrajectoryInterface.java
+++ javaapi/src/main/java/org/team2363/helixtrajectory/HelixTrajectoryInterface.java
@@ -4,9 +4,10 @@ public class HelixTrajectoryInterface {
 
     static {
         // System.loadLibrary("helixtrajectorycpp");
-        System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/casadi/libcasadi.dylib");
-        System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/casadi/libcasadi_nlpsol_ipopt.dylib");
-        System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/helixtrajectorycpp/build/lib/main/debug/libhelixtrajectorycpp.dylib");
+        // System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/casadi/libcasadi.dylib");
+        // System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/casadi/libcasadi_nlpsol_ipopt.dylib");
+        // System.load("/Users/jlbabilino/Documents/TripleHelix/Programming/Repositories/HelixTrajectoryJ/helixtrajectorycpp/build/lib/main/debug/libhelixtrajectorycpp.dylib");
+        System.loadLibrary("helixtrajectorycpp");
     }
 
     public native double[] generateTrajectory(double[] robotConfiguration, double[] path);
