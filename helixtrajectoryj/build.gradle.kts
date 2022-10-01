a79247a24876629b36e7fd0af580f4fdc2ea85c7
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2022-10-01 14:26:59 -0400

    Changed version

--- helixtrajectoryj/build.gradle.kts
+++ helixtrajectoryj/build.gradle.kts
@@ -26,7 +26,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.team2363"
             artifactId = "helixtrajectory" + project.properties["platform_id"];
-            version = "0.0.0-pre2"
+            version = "0.0.0-pre3"
 
             from(components["java"])
         }
