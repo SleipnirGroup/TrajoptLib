e14f016fdb76751be374913c4867003b17f109fa
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2022-12-27 22:08:01 -0500

    Interval Set solution checking system

--- helixtrajectorycpp/src/cpp/set/IntervalSet1d.cpp
+++ helixtrajectorycpp/src/cpp/set/IntervalSet1d.cpp
@@ -1,61 +1,59 @@
 #include "set/IntervalSet1d.h"
 
 #include <limits>
+#include <optional>
 
 #include <fmt/format.h>
 
-#include "IncompatibleTrajectoryException.h"
+#include "solution/SolutionChecking.h"
 
 namespace helixtrajectory {
 
-    IntervalSet1d::IntervalSet1d(double lower, double upper)
-            : lower(lower), upper(upper) {
-    }
+IntervalSet1d::IntervalSet1d(double lower, double upper)
+        : lower(lower), upper(upper) {
+}
 
-    IntervalSet1d::IntervalSet1d(double value)
-            : lower(value), upper(value) {
-    }
+IntervalSet1d::IntervalSet1d(double value)
+        : lower(value), upper(value) {
+}
 
-    IntervalSet1d IntervalSet1d::R1() {
-        return IntervalSet1d(
-                -std::numeric_limits<double>::infinity(),
-                +std::numeric_limits<double>::infinity());
-    }
+IntervalSet1d IntervalSet1d::R1() {
+    return IntervalSet1d(
+            -std::numeric_limits<double>::infinity(),
+            +std::numeric_limits<double>::infinity());
+}
 
-    bool IntervalSet1d::operator==(const IntervalSet1d& other) const noexcept {
-        return lower == other.lower && upper == other.upper;
-    }
+bool IntervalSet1d::operator==(const IntervalSet1d& other) const noexcept {
+    return lower == other.lower && upper == other.upper;
+}
 
-    double IntervalSet1d::Range() const noexcept {
-        return upper - lower;
-    }
+double IntervalSet1d::Range() const noexcept {
+    return upper - lower;
+}
 
-    bool IntervalSet1d::IsExact() const noexcept {
-        return lower == upper;
-    }
+bool IntervalSet1d::IsExact() const noexcept {
+    return lower == upper;
+}
 
-    bool IntervalSet1d::IsZero() const noexcept {
-        return lower == 0.0 && upper == 0.0;
-    }
+bool IntervalSet1d::IsZero() const noexcept {
+    return lower == 0.0 && upper == 0.0;
+}
 
-    bool IntervalSet1d::IsLowerBounded() const noexcept {
-        return lower > -std::numeric_limits<double>::infinity();
-    }
-    bool IntervalSet1d::IsUpperBounded() const noexcept {
-        return upper < +std::numeric_limits<double>::infinity();
-    }
+bool IntervalSet1d::IsLowerBounded() const noexcept {
+    return lower > -std::numeric_limits<double>::infinity();
+}
+bool IntervalSet1d::IsUpperBounded() const noexcept {
+    return upper < +std::numeric_limits<double>::infinity();
+}
 
-    void IntervalSet1d::CheckScalar(double scalar) const {
-        std::vector<bool> checks = GetConstraints(scalar);
-        for (bool check : checks) {
-            if (!check) {
-                throw IncompatibleTrajectoryException(
-                        fmt::format("{} is outside bound [{}, {}]", scalar, lower, upper));
-            }
-        }
+std::optional<SolutionError> IntervalSet1d::CheckScalar(double scalar, const SolutionTolerances& tolerances) const noexcept {
+    if (scalar < lower || scalar > upper) {
+        return SolutionError(fmt::format("{} is outside bound [{}, {}]", scalar, lower, upper));
     }
+    return std::nullopt;
+}
 
-    bool IntervalSet1d::IsValid() const noexcept {
-        return lower <= upper;
-    }
+bool IntervalSet1d::IsValid() const noexcept {
+    return lower <= upper;
+}
 }
