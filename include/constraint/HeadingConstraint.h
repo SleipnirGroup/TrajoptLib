bbb4bd5b75998bd106ab5c0b3234761d45910de4
Tyler Veness <calcmogul@gmail.com>
2023-06-06 09:21:03 -0700

    Update include/constraint/HeadingConstraint.h

--- include/constraint/HeadingConstraint.h
+++ include/constraint/HeadingConstraint.h
@@ -53,7 +53,7 @@ struct fmt::formatter<trajopt::HeadingConstraint> {
    * @param ctx Format string context.
    */
   auto format(const trajopt::HeadingConstraint& constraint,
-              fmt::format_context& ctx) {
+              fmt::format_context& ctx) const {
     return fmt::format_to(ctx.out(), "heading {}", constraint.headingBound);
   }
 };
