e92696cf020d7c17809c28ac7f4e51f7fd4567dc
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-06-06 14:49:52 -0400

    casadi 3.6.3 and macos arm

--- cmake/modules/FetchCasADi.cmake
+++ cmake/modules/FetchCasADi.cmake
@@ -1,15 +1,69 @@
 macro(fetch_casadi)
+  cmake_POLICY(SET CMP0135 NEW)
+  set(CASADI_LIBDIR ${CMAKE_BINARY_DIR}/_deps/casadi-src/casadi)
+  set(CASADI_INCLUDEDIR ${CMAKE_BINARY_DIR}/_deps/casadi-src/casadi/include)
   if (${CMAKE_SYSTEM_NAME} MATCHES "MINGW" OR ${CMAKE_SYSTEM_NAME} MATCHES "MSYS" OR WIN32)
     message(STATUS "Building for Windows")
-    set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.5.5/casadi-windows-py39-v3.5.5-64bit.zip)
+    set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.6.3/casadi-3.6.3-windows64-py311.zip)
+    set(CASADI_INSTALL_LIBS
+      ${CASADI_LIBDIR}/libcasadi.dll
+      ${CASADI_LIBDIR}/libstdc++-6.dll
+      ${CASADI_LIBDIR}/libcasadi_nlpsol_ipopt.dll
+      ${CASADI_LIBDIR}/libquadmath-0.dll
+      ${CASADI_LIBDIR}/libgcc_s_seh-1.dll)
+    set(CASADI_INSTALL_DEST "bin")
   elseif (APPLE)
-    message(STATUS "Building for macOS")
-    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};@loader_path/../lib;@loader_path")
-    set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.5.5/casadi-osx-py39-v3.5.5.tar.gz)
+    if (${CMAKE_APPLE_SILICON_PROCESSOR} MATCHES "arm64")
+      message(STATUS "Building for macOS arm64")
+      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};@loader_path/../lib;@loader_path")
+      set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.6.3/casadi-3.6.3-osx_arm64-py311.zip)
+      set(CASADI_INSTALL_LIBS
+        ${CASADI_LIBDIR}/libcasadi.3.7.dylib
+        ${CASADI_LIBDIR}/libc++.1.0.dylib
+        ${CASADI_LIBDIR}/libcasadi_nlpsol_ipopt.3.7.dylib
+        ${CASADI_LIBDIR}/libipopt.3.dylib
+        ${CASADI_LIBDIR}/libcoinmumps.3.dylib
+        ${CASADI_LIBDIR}/libcoinmetis.2.dylib
+        ${CASADI_LIBDIR}/libgfortran.5.dylib
+        ${CASADI_LIBDIR}/libquadmath.0.dylib
+        ${CASADI_LIBDIR}/libgcc_s.1.1.dylib)
+    else()
+      message(STATUS "Building for macOS x86_64")
+      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};@loader_path/../lib;@loader_path")
+      set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.6.3/casadi-3.6.3-osx64-py311.zip)
+      set(CASADI_INSTALL_LIBS
+        ${CASADI_LIBDIR}/libcasadi.3.7.dylib
+        ${CASADI_LIBDIR}/libc++.1.0.dylib
+        ${CASADI_LIBDIR}/libcasadi_nlpsol_ipopt.3.7.dylib
+        ${CASADI_LIBDIR}/libipopt.3.dylib
+        ${CASADI_LIBDIR}/libcoinmumps.3.dylib
+        ${CASADI_LIBDIR}/libcoinmetis.2.dylib
+        ${CASADI_LIBDIR}/libgfortran.5.dylib
+        ${CASADI_LIBDIR}/libquadmath.0.dylib
+        ${CASADI_LIBDIR}/libgcc_s.1.dylib
+        ${CASADI_LIBDIR}/libgcc_s.1.1.dylib)
+    endif()
+    set(CASADI_INSTALL_DEST "lib")
   elseif (UNIX)
-    message(STATUS "Building for Linux")
-    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};$ORIGIN/../lib;$ORIGIN")
-    set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.5.5/libcasadi-linux-gcc5-v3.5.5.tar.gz)
+    if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "ARM64")
+      message(STATUS "Building for Linux arm64")
+      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};$ORIGIN/../lib;$ORIGIN")
+      set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.6.3/casadi-3.6.3-linux-aarch64-py311.zip)
+      set(CASADI_INSTALL_LIBS
+        ${CASADI_LIBDIR}/libcasadi.so
+        ${CASADI_LIBDIR}/libcasadi_nlpsol_ipopt.so)
+    else()
+      message(STATUS "Building for Linux x64")
+      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};$ORIGIN/../lib;$ORIGIN")
+      set(CASADI_URL https://github.com/casadi/casadi/releases/download/3.5.5/libcasadi-linux-gcc5-v3.5.5.tar.gz)
+      set(CASADI_LIBDIR ${CMAKE_BINARY_DIR}/_deps/casadi-src/lib)
+      set(CASADI_INCLUDEDIR ${CMAKE_BINARY_DIR}/_deps/casadi-src/include)
+      set(CASADI_INSTALL_LIBS
+        ${CASADI_LIBDIR}/libcasadi.so
+        ${CASADI_LIBDIR}/libcasadi_nlpsol_ipopt.so)
+    endif()
+    add_definitions(-D_GLIBCXX_USE_CXX11_ABI=0)
+    set(CASADI_INSTALL_DEST "lib")
   endif()
   message(STATUS "Downloading CasADi from ${CASADI_URL}")
 
