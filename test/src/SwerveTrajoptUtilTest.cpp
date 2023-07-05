529fc9ad9fa218264f3859ddef4def1018c468a3
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-07-05 10:46:57 -0400

    Reorder tests SwerveTrajoptUtil

--- test/src/SwerveTrajoptUtilTest.cpp
+++ test/src/SwerveTrajoptUtilTest.cpp
@@ -12,19 +12,6 @@
 #include "trajopt/path/InitialGuessPoint.h"
 #include "trajopt/set/IntervalSet1d.h"
 
-TEST(SwerveTrajoptUtilTest, ApplyKinematicsConstraints) {
-  TestOpti opti;
-  //                         0      0      1      0      1      2     3
-  std::vector<double> x{1.0, -1.0, 9.0, -19.0, 33.0, 101.0, 41.0};
-  std::vector<double> v{1.0, -1.0, 5.0, -7.0, 13.0, 17.0, -15.0};
-  std::vector<double> a{1.0, -1.0, 3.0, -3.0, 5.0, 1.0, -8.0};
-  //                  dt{         2.0,   2.0,   4.0,   4.0,   4.0,   4.0}
-  std::vector<double> dt{2.0, 4.0};
-  std::vector<size_t> N{2, 4};
-  trajopt::ApplyKinematicsConstraints(opti, x, x, x, v, v, v, a, a, a, dt, N);
-  EXPECT_FALSE(opti.IsViolating());
-}
-
 TEST(SwerveTrajoptUtilTest, SolveNetForce) {
   std::vector<double> Fx{4.0, 1.0, 3.0, 5.0};
   std::vector<double> Fy{0.0, 2.0, -5.0, 2.0};
@@ -51,4 +38,17 @@ TEST(SwerveTrajoptUtilTest, SolveNetTorque) {
   double tau_net = std::accumulate(tau.begin(), tau.end(), 0.0);
   double solved_tau_net = trajopt::SolveNetTorque(theta, Fx, Fy, swerveModules);
   EXPECT_NEAR(solved_tau_net, tau_net, 0.001);
+}
+
+TEST(SwerveTrajoptUtilTest, ApplyKinematicsConstraints) {
+  TestOpti opti;
+  //                         0      0      1      0      1      2     3
+  std::vector<double> x{1.0, -1.0, 9.0, -19.0, 33.0, 101.0, 41.0};
+  std::vector<double> v{1.0, -1.0, 5.0, -7.0, 13.0, 17.0, -15.0};
+  std::vector<double> a{1.0, -1.0, 3.0, -3.0, 5.0, 1.0, -8.0};
+  //                  dt{         2.0,   2.0,   4.0,   4.0,   4.0,   4.0}
+  std::vector<double> dt{2.0, 4.0};
+  std::vector<size_t> N{2, 4};
+  trajopt::ApplyKinematicsConstraints(opti, x, x, x, v, v, v, a, a, a, dt, N);
+  EXPECT_FALSE(opti.IsViolating());
 }
