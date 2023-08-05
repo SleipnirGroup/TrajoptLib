63be9e5b0e3e66447f4f4a1424b80fe2aac43478
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-08-04 21:05:34 -0400

    wpiformat run

--- rust/include/trajoptlib.h
+++ rust/include/trajoptlib.h
@@ -2,12 +2,11 @@
 
 #pragma once
 
+#include <rust/cxx.h>
 #include <trajopt/path/SwervePathBuilder.h>
 
 #include <cstddef>
 #include <memory>
-
-#include <rust/cxx.h>
 
 namespace trajoptlibrust {
 
@@ -23,9 +22,11 @@ class SwervePathBuilderImpl {
 
   void pose_wpt(size_t idx, double x, double y, double heading);
   void translation_wpt(size_t idx, double x, double y, double heading_guess);
-  void empty_wpt(size_t idx, double x_guess, double y_guess, double heading_guess);
+  void empty_wpt(size_t idx, double x_guess, double y_guess,
+                 double heading_guess);
 
-  void sgmt_initial_guess_points(size_t from_idx, const rust::Vec<InitialGuessPoint>& guess_points);
+  void sgmt_initial_guess_points(
+      size_t from_idx, const rust::Vec<InitialGuessPoint>& guess_points);
 
   void wpt_linear_velocity_direction(size_t idx, double angle);
   void wpt_linear_velocity_max_magnitude(size_t idx, double magnitude);
@@ -35,15 +36,20 @@ class SwervePathBuilderImpl {
   void wpt_y(size_t idx, double y);
   void wpt_heading(size_t idx, double heading);
 
-  void sgmt_linear_velocity_direction(size_t from_idx, size_t to_idx, double angle);
-  void sgmt_linear_velocity_max_magnitude(size_t from_idx, size_t to_idx, double magnitude);
-  void sgmt_linear_velocity_polar(size_t from_idx, size_t to_idx, double magnitude, double angle);
-  void sgmt_angular_velocity(size_t from_idx, size_t to_idx, double angular_velocity);
+  void sgmt_linear_velocity_direction(size_t from_idx, size_t to_idx,
+                                      double angle);
+  void sgmt_linear_velocity_max_magnitude(size_t from_idx, size_t to_idx,
+                                          double magnitude);
+  void sgmt_linear_velocity_polar(size_t from_idx, size_t to_idx,
+                                  double magnitude, double angle);
+  void sgmt_angular_velocity(size_t from_idx, size_t to_idx,
+                             double angular_velocity);
   void sgmt_x(size_t from_idx, size_t to_idx, double x);
   void sgmt_y(size_t from_idx, size_t to_idx, double y);
   void sgmt_heading(size_t from_idx, size_t to_idx, double heading);
 
-  void sgmt_circle_obstacle(size_t from_idx, size_t to_idx, double x, double y, double radius);
+  void sgmt_circle_obstacle(size_t from_idx, size_t to_idx, double x, double y,
+                            double radius);
 
   HolonomicTrajectory generate() const;
 
