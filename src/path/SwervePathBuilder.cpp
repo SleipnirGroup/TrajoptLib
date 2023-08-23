51b176b93b9fd417a06b2f058aa45f27a9d716b1
shueja-personal <32416547+shueja-personal@users.noreply.github.com>
2023-08-23 16:20:12 -0700

    remove error throw

--- src/path/SwervePathBuilder.cpp
+++ src/path/SwervePathBuilder.cpp
@@ -75,8 +75,7 @@ void SwervePathBuilder::WptVelocityDirection(size_t idx, double angle) {
 }
 
 void SwervePathBuilder::WptVelocityMagnitude(size_t idx, double v) {
-  std::cerr << v << std::endl;
-  throw std::invalid_argument("crash");
+  std::cout << v << std::endl;
   if (std::abs(v) < 0.0001) {
     WptZeroVelocity(idx);
   } else {
