ee43eb6cb35b10d41d38208e1be7110bc9a73520
shueja-personal <j.a.shue@outlook.com>
2023-11-13 22:49:19 -0800

    more fix sparsity

--- src/optimization/CasADiIterCallback.h
+++ src/optimization/CasADiIterCallback.h
@@ -1,5 +1,6 @@
 #include "casadi/casadi.hpp"
 #include "casadi/core/nlpsol.hpp"
+#include "casadi/core/sparsity.hpp"
 using namespace casadi;
 class CasADiIterCallback : public Callback {
   // Data members
@@ -20,18 +21,18 @@ public:
   // Number of inputs and outputs
   casadi_int get_n_in() override { return 6;}
   casadi_int get_n_out() override { return 1;}
-  Sparsity get_sparsity_in(casadi_int i) {
+  Sparsity get_sparsity_in(casadi_int i) override {
     switch (static_cast<NlpsolOutput>(i)) {
     case NLPSOL_F:
-      return Sparsity.scalar();
+      return Sparsity::scalar();
     case NLPSOL_X:
     case NLPSOL_LAM_X:
-      return Sparsity.dense(nx);
+      return Sparsity::dense(nx);
     case NLPSOL_LAM_G:
     case NLPSOL_G:
-        return Sparsity.dense(ng);
+        return Sparsity::dense(ng);
     case NLPSOL_LAM_P:
-      return Sparsity.dense(np);
+      return Sparsity::dense(np);
     case NLPSOL_NUM_OUT: break;
     }
     return Sparsity();
