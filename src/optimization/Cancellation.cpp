56dc0b8e5b3d5717efb95071ca05e18af5c2d6da
shueja-personal <j.a.shue@outlook.com>
2023-11-14 14:10:18 -0800

    namespace

--- src/optimization/Cancellation.cpp
+++ src/optimization/Cancellation.cpp
@@ -1,6 +1,8 @@
     #include <atomic>
     #include "optimization/Cancellation.h"
+    namespace trajopt {
     std::atomic<int>& GetCancellationFlag() {
         static std::atomic<int> flag;
         return flag;
+    }
     }
