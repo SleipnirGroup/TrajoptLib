075ef7b63212f7d67c188d8685099235d6f316ab
shueja-personal <j.a.shue@outlook.com>
2023-11-14 14:23:19 -0800

    init flag to 0

--- src/optimization/Cancellation.cpp
+++ src/optimization/Cancellation.cpp
@@ -2,7 +2,7 @@
     #include "optimization/Cancellation.h"
     namespace trajopt {
     std::atomic<int>& GetCancellationFlag() {
-        static std::atomic<int> flag;
+        static std::atomic<int> flag(0);
         return flag;
     }
     }
