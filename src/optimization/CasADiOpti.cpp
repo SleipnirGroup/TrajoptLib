52af6ed7a18e576445227e29a4dc9aec81e9fa9b
shueja-personal <j.a.shue@outlook.com>
2023-11-14 11:10:39 -0800

    make const heap allocation

--- src/optimization/CasADiOpti.cpp
+++ src/optimization/CasADiOpti.cpp
@@ -24,7 +24,7 @@ void CasADiOpti::SetInitial(const casadi::MX& expression, double value) {
   opti.set_initial(expression, value);
 }
 void CasADiOpti::Solve() {
-  const auto callback = new CasADiIterCallback("f", opti.nx(), opti.ng(), opti.np());
+  const auto callback = new const CasADiIterCallback("f", opti.nx(), opti.ng(), opti.np());
 #ifdef DEBUG_OUTPUT
   // I don't try-catch this next line since it should always work.
   // I'm assuming the dynamic lib is on the path and casadi can find it.
