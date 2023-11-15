3c6490aab53ecf52569964b298b2deda85994622
shueja <32416547+shueja@users.noreply.github.com>
2023-11-15 14:10:49 -0800

    Update src/optimization/Cancellation.cpp
    Co-authored-by: Tyler Veness <calcmogul@gmail.com>

--- src/optimization/Cancellation.cpp
+++ src/optimization/Cancellation.cpp
@@ -5,8 +5,10 @@
 #include <atomic>
 
 namespace trajopt {
+
 std::atomic<int>& GetCancellationFlag() {
-  static std::atomic<int> flag(0);
+  static std::atomic<int> flag{0};
   return flag;
 }
+
 }  // namespace trajopt
