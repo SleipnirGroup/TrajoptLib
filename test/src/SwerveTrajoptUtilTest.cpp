e15f2923d9c03c804798796d212fd7d31b5189c3
Tyler Veness <calcmogul@gmail.com>
2024-02-18 13:14:56 -0800

    Refactor test

--- test/src/SwerveTrajoptUtilTest.cpp
+++ test/src/SwerveTrajoptUtilTest.cpp
@@ -4,41 +4,60 @@
 #include <numeric>
 #include <vector>
 
-#include <gtest/gtest.h>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <trajopt/path/InitialGuessPoint.h>
+#include <trajopt/set/IntervalSet1d.h>
 
 #include "TestOpti.h"
 #include "optimization/SwerveTrajoptUtil.h"
 #include "optimization/TrajoptUtil.h"
-#include "trajopt/path/InitialGuessPoint.h"
-#include "trajopt/set/IntervalSet1d.h"
 
-TEST(SwerveTrajoptUtilTest, SolveNetForce) {
+TEST_CASE("SwerveTrajoptUtil - SolveNetForce()", "[SwerveTrajoptUtil]") {
   std::vector<double> Fx{4.0, 1.0, 3.0, 5.0};
   std::vector<double> Fy{0.0, 2.0, -5.0, 2.0};
+
   auto [Fx_net, Fy_net] = trajopt::SolveNetForce(Fx, Fy);
-  EXPECT_DOUBLE_EQ(Fx_net, 13.0);
-  EXPECT_DOUBLE_EQ(Fy_net, -1.0);
+
+  CHECK(Fx_net == Catch::Approx(13.0).margin(1e-3));
+  CHECK(Fy_net == Catch::Approx(-1.0).margin(1e-3));
 }
 
-TEST(SwerveTrajoptUtilTest, SolveNetTorque) {
-  double theta = 1.0;
+TEST_CASE("SwerveTrajoptUtil - SolveNetTorque()", "[SwerveTrajoptUtil]") {
+  constexpr double theta = 1.0;
+
   std::vector<double> Fx{-3.0, -5.0, -4.0, 2.0};
   std::vector<double> Fy{4.0, -5.0, -2.0, -4.0};
   std::vector<trajopt::SwerveModule> swerveModules = {
-    {.x =  1.0, .y =  1.0, .wheelRadius = 0.0, .wheelMaxAngularVelocity = 0.0,
-        .wheelMaxTorque = 0.0},
-    {.x =  1.0, .y = -1.0, .wheelRadius = 0.0, .wheelMaxAngularVelocity = 0.0,
-        .wheelMaxTorque = 0.0},
-    {.x = -1.0, .y =  1.0, .wheelRadius = 0.0, .wheelMaxAngularVelocity = 0.0,
-        .wheelMaxTorque = 0.0},
-    {.x = -1.0, .y = -1.0, .wheelRadius = 0.0, .wheelMaxAngularVelocity = 0.0,
-        .wheelMaxTorque = 0.0},
+      {.x = 1.0,
+       .y = 1.0,
+       .wheelRadius = 0.0,
+       .wheelMaxAngularVelocity = 0.0,
+       .wheelMaxTorque = 0.0},
+      {.x = 1.0,
+       .y = -1.0,
+       .wheelRadius = 0.0,
+       .wheelMaxAngularVelocity = 0.0,
+       .wheelMaxTorque = 0.0},
+      {.x = -1.0,
+       .y = 1.0,
+       .wheelRadius = 0.0,
+       .wheelMaxAngularVelocity = 0.0,
+       .wheelMaxTorque = 0.0},
+      {.x = -1.0,
+       .y = -1.0,
+       .wheelRadius = 0.0,
+       .wheelMaxAngularVelocity = 0.0,
+       .wheelMaxTorque = 0.0},
   };
+
   double tau_net = trajopt::SolveNetTorque(theta, Fx, Fy, swerveModules);
-  EXPECT_NEAR(tau_net, 0.6553658, 0.001);
+
+  CHECK(tau_net == Catch::Approx(0.6553658).margin(1e-3));
 }
 
-TEST(SwerveTrajoptUtilTest, ApplyKinematicsConstraints) {
+TEST_CASE("SwerveTrajoptUtil - ApplyKinematicsConstraints()",
+          "[SwerveTrajoptUtil]") {
   TestOpti opti;
   //                         0      0      1      0      1      2     3
   std::vector<double> x{1.0, -1.0, 9.0, -19.0, 33.0, 101.0, 41.0};
@@ -47,21 +66,26 @@ TEST(SwerveTrajoptUtilTest, ApplyKinematicsConstraints) {
   //                  dt{         2.0,   2.0,   4.0,   4.0,   4.0,   4.0}
   std::vector<double> dt{2.0, 4.0};
   std::vector<size_t> N{2, 4};
+
   trajopt::ApplyKinematicsConstraints(opti, x, x, x, v, v, v, a, a, a, dt, N);
-  EXPECT_FALSE(opti.IsViolating());
+
+  CHECK_FALSE(opti.IsViolating());
 }
 
-// Uncomment when fix is made for this function in future PR
-// TEST(SwerveTrajoptUtilTest, ApplyDynamicsConstraints) {
-//   TestOpti opti;
-//   double ax = 1.0;
-//   double ay = -2.0;
-//   double alpha = 9.0;
-//   double Fx_net = 45.0;
-//   double Fy_net = -90.0;
-//   double tau_net = 54.0;
-//   double mass = 45.0;
-//   double moi = 6.0;
-//   trajopt::ApplyDynamicsConstraints(opti, ax, ay, alpha, Fx_net, Fy_net, tau_net, mass, moi);
-//   EXPECT_FALSE(opti.IsViolating());
-// }
+TEST_CASE("SwerveTrajoptUtil - ApplyDynamicsConstraints()",
+          "[SwerveTrajoptUtil]") {
+  TestOpti opti;
+  constexpr double ax = 1.0;
+  constexpr double ay = -2.0;
+  constexpr double alpha = 9.0;
+  constexpr double Fx_net = 45.0;
+  constexpr double Fy_net = -90.0;
+  constexpr double tau_net = 54.0;
+  constexpr double mass = 45.0;
+  constexpr double moi = 6.0;
+
+  trajopt::ApplyDynamicsConstraints(opti, ax, ay, alpha, Fx_net, Fy_net,
+                                    tau_net, mass, moi);
+
+  CHECK_FALSE(opti.IsViolating());
+}
