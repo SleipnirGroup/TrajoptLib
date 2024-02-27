6bc87722332fc1182bcffa3e692174fc991f436e
Tyler Veness <calcmogul@gmail.com>
2024-02-27 15:58:08 -0800

    Remove Java bindings
    Choreo doesn't use it, and CI doesn't build the JNI.

--- java/src/main/java/org/sleipnirgroup/trajopt/HolonomicDrivetrain.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) TrajoptLib contributors
-
-package org.sleipnirgroup.trajopt;
-
-public abstract class HolonomicDrivetrain extends Drivetrain {
-    protected HolonomicDrivetrain(double mass, double momentOfInertia, Obstacle bumpers) throws NullPointerException {
-        super(mass, momentOfInertia, bumpers);
-    }
-}
