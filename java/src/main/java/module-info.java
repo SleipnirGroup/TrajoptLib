f9ca0f23c545f23c03b79f6d937fe2028108fc2b
Tyler Veness <calcmogul@gmail.com>
2024-02-27 16:18:44 -0800

    Remove Java bindings (#114)
    Choreo doesn't use it, and CI doesn't build the JNI.

--- java/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) TrajoptLib contributors
-
-module org.sleipnirgroup.trajopt {
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    exports org.sleipnirgroup.trajopt;
-}
--- java/src/main/java/org/sleipnirgroup/trajopt/PluginLoadException.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) TrajoptLib contributors
-
-package org.sleipnirgroup.trajopt;
-
-public class PluginLoadException extends Exception {
-    public PluginLoadException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
