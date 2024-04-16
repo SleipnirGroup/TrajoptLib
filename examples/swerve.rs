6a311f69c70d12fd1339ca46e2c423a95f5f4195
camaj <joshua.g.camarena@gmail.com>
2024-04-15 20:35:09 -0700

    Update swerve.rs

--- examples/swerve.rs
+++ examples/swerve.rs
@@ -2,7 +2,7 @@ use trajoptlib::{SwerveDrivetrain, SwerveModule, SwervePathBuilder};
 
 fn main() {
     let drivetrain = SwerveDrivetrain {
-        mass: 45.0,
+        mass: 80.0,
         moi: 6.0,
         modules: vec![
             SwerveModule {
@@ -10,28 +10,28 @@ fn main() {
                 y: 0.6,
                 wheel_radius: 0.04,
                 wheel_max_angular_velocity: 70.0,
-                wheel_max_torque: 2.0,
+                wheel_max_torque: 0.9,
             },
             SwerveModule {
                 x: 0.6,
                 y: -0.6,
                 wheel_radius: 0.04,
                 wheel_max_angular_velocity: 70.0,
-                wheel_max_torque: 2.0,
+                wheel_max_torque: 0.9,
             },
             SwerveModule {
                 x: -0.6,
                 y: 0.6,
                 wheel_radius: 0.04,
                 wheel_max_angular_velocity: 70.0,
-                wheel_max_torque: 2.0,
+                wheel_max_torque: 0.9,
             },
             SwerveModule {
                 x: -0.6,
                 y: -0.6,
                 wheel_radius: 0.04,
                 wheel_max_angular_velocity: 70.0,
-                wheel_max_torque: 2.0,
+                wheel_max_torque: 0.9,
             },
         ],
     };
@@ -41,13 +41,15 @@ fn main() {
     path.set_drivetrain(&drivetrain);
     path.set_bumpers(1.3, 1.3);
     path.pose_wpt(0, 0.0, 0.0, 0.0);
-    path.pose_wpt(1, 1.0, 0.0, 0.0);
+    path.pose_wpt(1, 1.0, 0.0, 0.);
     // path.wpt_linear_velocity_polar(0, 0.0, 0.0);
     // path.wpt_linear_velocity_polar(1, 0.0, 0.0);
     // path.wpt_angular_velocity(0, 0.0);
     // path.wpt_angular_velocity(1, 0.0);
     // path.sgmt_circle_obstacle(0, 1, 0.5, 0.1, 0.2);
-    path.set_control_interval_counts(vec![40]);
+    path.set_control_interval_counts(vec![3]);
     println!("setup complete");
-    println!("{:?}", path.generate(true, 0));
+    // println!("linear: {:#?}", path.calculate_linear_initial_guess());
+    // println!("spline: {:#?}", path.calculate_spline_initial_guess());
+    println!("{:#?}", path.generate(true, 0));
 }
