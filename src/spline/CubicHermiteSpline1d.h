b8e51a8790e4e3fcd35f34150da79b71126c1f80
camaj <joshua.g.camarena@gmail.com>
2024-04-15 20:35:44 -0700

    add spline1d

---
+++ src/spline/CubicHermiteSpline1d.h
@@ -0,0 +1,34 @@
+#include <cmath>
+#include "spline/Spline1d.h"
+
+namespace trajopt {
+    
+class CubicHermiteSpline1d : public Spline1d {
+public:
+    // Coefficients of the cubic polynomial
+    const double a, b, c, d;
+
+    CubicHermiteSpline1d(double p0, double p1, double v0, double v1)
+    : a(v0 + v1 + 2 * p0 - 2 * p1),
+      b(-2 * v0 - v1 - 3 * p0 + 3 * p1),
+      c(v0),
+      d(p0) {}
+
+    double getPosition(double t) const override {
+        return a * std::pow(t, 3) + b * std::pow(t, 2) + c * t + d;
+    }
+
+    double getVelocity(double t) const override {
+        return 3 * a * std::pow(t, 2) + 2 * b * t + c;
+    }
+
+    double getAcceleration(double t) const override {
+        return 6 * a * t + 2 * b;
+    }
+
+    double getJerk(double t) const override {
+        return 6 * a;
+    }
+};
+
+} // namespace trajopt
