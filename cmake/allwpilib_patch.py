f76508ea83356154dfee8fddc7b69e00bc327625
Tyler Veness <calcmogul@gmail.com>
2024-04-23 19:02:10 -0700

    Make wpiutil emscripten backtrace a no-op

---
+++ cmake/allwpilib_patch.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import subprocess
+import sys
+
+
+def make_emscripten_backtrace_a_no_op():
+    subprocess.run(
+        [
+            "git",
+            "apply",
+            "--ignore-whitespace",
+            os.path.join(
+                sys.argv[1], "cmake/allwpilib-make-emscripten-backtrace-a-no-op.patch"
+            ),
+        ],
+        check=True,
+    )
+
+
+def remove_protobuf_support():
+    shutil.rmtree("wpimath/src/main/native/cpp/controller/proto", ignore_errors=True)
+    shutil.rmtree("wpimath/src/main/native/cpp/geometry/proto", ignore_errors=True)
+    shutil.rmtree("wpimath/src/main/native/cpp/kinematics/proto", ignore_errors=True)
+    shutil.rmtree("wpimath/src/main/native/cpp/system/plant/proto", ignore_errors=True)
+    shutil.rmtree("wpimath/src/main/native/cpp/trajectory/proto", ignore_errors=True)
+    shutil.rmtree("wpiutil/src/main/native/cpp/protobuf", ignore_errors=True)
+    shutil.rmtree(
+        "wpimath/src/main/native/include/frc/controller/proto", ignore_errors=True
+    )
+    shutil.rmtree(
+        "wpimath/src/main/native/include/frc/geometry/proto", ignore_errors=True
+    )
+    shutil.rmtree(
+        "wpimath/src/main/native/include/frc/kinematics/proto", ignore_errors=True
+    )
+    shutil.rmtree(
+        "wpimath/src/main/native/include/frc/system/plant/proto", ignore_errors=True
+    )
+    shutil.rmtree(
+        "wpimath/src/main/native/include/frc/trajectory/proto", ignore_errors=True
+    )
+    shutil.rmtree("wpiutil/src/main/native/thirdparty/protobuf", ignore_errors=True)
+    subprocess.run(
+        [
+            "git",
+            "apply",
+            "--ignore-whitespace",
+            os.path.join(sys.argv[1], "cmake/allwpilib-remove-protobuf-support.patch"),
+        ],
+        check=True,
+    )
+
+
+def main():
+    make_emscripten_backtrace_a_no_op()
+    remove_protobuf_support()
+
+
+if __name__ == "__main__":
+    main()
