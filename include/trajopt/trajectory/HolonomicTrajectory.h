780ab99d60aa696b830ff9696e82f35be874326b
Ben Scholar <scholar@case.edu>
2024-05-23 20:24:08 -0700

    Expose module forces (#167)

--- include/trajopt/trajectory/HolonomicTrajectory.h
+++ include/trajopt/trajectory/HolonomicTrajectory.h
@@ -7,6 +7,7 @@
 
 #include "trajopt/SymbolExports.h"
 #include "trajopt/solution/HolonomicSolution.h"
+#include "trajopt/solution/SwerveSolution.h"
 #include "trajopt/trajectory/HolonomicTrajectorySample.h"
 
 namespace trajopt {
@@ -43,6 +44,24 @@ class TRAJOPT_DLLEXPORT HolonomicTrajectory {
       samples.emplace_back(ts, solution.x[samp], solution.y[samp],
                            solution.theta[samp], solution.vx[samp],
                            solution.vy[samp], solution.omega[samp]);
+    }
+  }
+
+  /**
+   * Construct a HolonomicTrajectory from a swerve solution.
+   *
+   * @param solution The swerve solution.
+   */
+  explicit HolonomicTrajectory(const SwerveSolution& solution) {
+    double ts = 0.0;
+    for (size_t samp = 0; samp < solution.x.size(); ++samp) {
+      if (samp != 0) {
+        ts += solution.dt[samp - 1];
+      }
+      samples.emplace_back(ts, solution.x[samp], solution.y[samp],
+                           solution.theta[samp], solution.vx[samp],
+                           solution.vy[samp], solution.omega[samp],
+                           solution.moduleFX[samp], solution.moduleFY[samp]);
     }
   }
 };
--- include/trajopt/trajectory/HolonomicTrajectorySample.h
+++ include/trajopt/trajectory/HolonomicTrajectorySample.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+#include <utility>
+#include <vector>
+
 #include "trajopt/SymbolExports.h"
 
 namespace trajopt {
@@ -32,7 +35,13 @@ class TRAJOPT_DLLEXPORT HolonomicTrajectorySample {
   /// The angular velocity.
   double angularVelocity = 0.0;
 
-  constexpr HolonomicTrajectorySample() = default;
+  /// The force on each module in the X direction.
+  std::vector<double> moduleForcesX;
+
+  /// The force on each module in the Y direction.
+  std::vector<double> moduleForcesY;
+
+  HolonomicTrajectorySample() = default;
 
   /**
    * Construct a HolonomicTrajectorySample.
@@ -45,9 +54,9 @@ class TRAJOPT_DLLEXPORT HolonomicTrajectorySample {
    * @param velocityY The velocity's y component.
    * @param angularVelocity The angular velocity.
    */
-  constexpr HolonomicTrajectorySample(double timestamp, double x, double y,
-                                      double heading, double velocityX,
-                                      double velocityY, double angularVelocity)
+  HolonomicTrajectorySample(double timestamp, double x, double y,
+                            double heading, double velocityX, double velocityY,
+                            double angularVelocity)
       : timestamp{timestamp},
         x{x},
         y{y},
@@ -55,6 +64,34 @@ class TRAJOPT_DLLEXPORT HolonomicTrajectorySample {
         velocityX{velocityX},
         velocityY{velocityY},
         angularVelocity{angularVelocity} {}
+
+  /**
+   * Construct a HolonomicTrajectorySample with module forces.
+   *
+   * @param timestamp The timestamp.
+   * @param x The x coordinate.
+   * @param y The y coordinate.
+   * @param heading The heading.
+   * @param velocityX The velocity's x component.
+   * @param velocityY The velocity's y component.
+   * @param angularVelocity The angular velocity.
+   * @param moduleForcesX Forces acting on the modules in the X direction.
+   * @param moduleForcesY Forces acting on the modules in the Y direction.
+   */
+  HolonomicTrajectorySample(double timestamp, double x, double y,
+                            double heading, double velocityX, double velocityY,
+                            double angularVelocity,
+                            std::vector<double> moduleForcesX,
+                            std::vector<double> moduleForcesY)
+      : timestamp{timestamp},
+        x{x},
+        y{y},
+        heading{heading},
+        velocityX{velocityX},
+        velocityY{velocityY},
+        angularVelocity{angularVelocity},
+        moduleForcesX{std::move(moduleForcesX)},
+        moduleForcesY{std::move(moduleForcesY)} {}
 };
 
 }  // namespace trajopt
