29560f8241b7d077382c762e7d62fc49cdfd4880
Tyler Veness <calcmogul@gmail.com>
2024-06-18 22:45:26 -0700

    Merge .inc files into .h files
    This fixes clangd's header parsing and makes the function
    implementations easier to find.

--- src/optimization/HolonomicTrajoptUtil.h
+++ src/optimization/HolonomicTrajoptUtil.h
@@ -2,12 +2,13 @@
 
 #pragma once
 
-#include <memory>
-#include <vector>
-
 #include "optimization/TrajoptUtil.h"
-#include "trajopt/obstacle/Obstacle.h"
-#include "trajopt/solution/HolonomicSolution.h"
+#include "trajopt/constraint/HeadingConstraint.h"
+#include "trajopt/constraint/LinePointConstraint.h"
+#include "trajopt/constraint/PointAtConstraint.h"
+#include "trajopt/constraint/PointLineConstraint.h"
+#include "trajopt/constraint/PointPointConstraint.h"
+#include "trajopt/constraint/TranslationConstraint.h"
 
 namespace trajopt {
 
@@ -17,7 +18,56 @@ void ApplyHolonomicConstraint(Opti& opti, const Expr& x, const Expr& y,
                               const Expr& theta, const Expr& vx, const Expr& vy,
                               const Expr& omega, const Expr& ax, const Expr& ay,
                               const Expr& alpha,
-                              const HolonomicConstraint& constraint);
-}  // namespace trajopt
+                              const HolonomicConstraint& constraint) {
+  if (std::holds_alternative<HolonomicVelocityConstraint>(constraint)) {
+    const auto& velocityHolonomicConstraint =
+        std::get<HolonomicVelocityConstraint>(constraint);
+    ApplySet2dConstraint(opti, vx, vy,
+                         velocityHolonomicConstraint.velocityBound);
+  } else if (std::holds_alternative<AngularVelocityConstraint>(constraint)) {
+    const auto& angularVelocityConstraint =
+        std::get<AngularVelocityConstraint>(constraint);
+    ApplyIntervalSet1dConstraint(
+        opti, omega, angularVelocityConstraint.angularVelocityBound);
+  } else if (std::holds_alternative<TranslationConstraint>(constraint)) {
+    ApplyConstraint(opti, x, y, theta,
+                    std::get<TranslationConstraint>(constraint));
+  } else if (std::holds_alternative<HeadingConstraint>(constraint)) {
+    ApplyConstraint(opti, x, y, theta, std::get<HeadingConstraint>(constraint));
+  } else if (std::holds_alternative<LinePointConstraint>(constraint)) {
+    ApplyConstraint(opti, x, y, theta,
+                    std::get<LinePointConstraint>(constraint));
+  } else if (std::holds_alternative<PointAtConstraint>(constraint)) {
+    auto pointAtConstraint = std::get<PointAtConstraint>(constraint);
+    double fieldPointX = pointAtConstraint.fieldPointX;
+    double fieldPointY = pointAtConstraint.fieldPointY;
+    double headingTolerance = pointAtConstraint.headingTolerance;
+    /**
+     * dx,dy = desired heading
+     * ux,uy = unit vector of desired heading
+     * hx,hy = heading
+     * dot = dot product of ux,uy and hx,hy
+     *
+     * constrain dot to cos(1.0), which is colinear
+     * and cos(thetaTolerance)
+     */
+    auto dx = fieldPointX - x;
+    auto dy = fieldPointY - y;
+    auto ux = dx / hypot(dx, dy);  // NOLINT
+    auto uy = dy / hypot(dx, dy);  // NOLINT
+    auto hx = cos(theta);          // NOLINT
+    auto hy = sin(theta);          // NOLINT
+    auto dot = hx * ux + hy * uy;
 
-#include "optimization/HolonomicTrajoptUtil.inc"
+    ApplyIntervalSet1dConstraint(
+        opti, dot, IntervalSet1d(std::cos(headingTolerance), 1.0));
+  } else if (std::holds_alternative<PointLineConstraint>(constraint)) {
+    ApplyConstraint(opti, x, y, theta,
+                    std::get<PointLineConstraint>(constraint));
+  } else if (std::holds_alternative<PointPointConstraint>(constraint)) {
+    ApplyConstraint(opti, x, y, theta,
+                    std::get<PointPointConstraint>(constraint));
+  }  // TODO: Investigate a way to condense the code above
+}
+
+}  // namespace trajopt
