69c1c02a5dddfb19d912f39ec2846020750c0ff0
Tyler Veness <calcmogul@gmail.com>
2024-06-18 23:27:21 -0700

    Reduce macro usage in OptimalTrajectoryGenerator.cpp

--- src/OptimalTrajectoryGenerator.cpp
+++ src/OptimalTrajectoryGenerator.cpp
@@ -2,29 +2,26 @@
 
 #include "trajopt/OptimalTrajectoryGenerator.h"
 
-#include <stdint.h>
-
-#include "trajopt/expected"
-#include "trajopt/path/SwervePathBuilder.h"
-
 #if defined(OPTIMIZER_BACKEND_CASADI)
 #include "optimization/CasADiOpti.h"
-#define _OPTI_BACKEND casadi::MX, CasADiOpti
 #elif defined(OPTIMIZER_BACKEND_SLEIPNIR)
 #include "optimization/SleipnirOpti.h"
-#define _OPTI_BACKEND SleipnirExpr, SleipnirOpti
 #endif
-#include "trajopt/drivetrain/SwerveDrivetrain.h"
+
 #include "optimization/algorithms/SwerveDiscreteOptimal.h"
-#include "trajopt/solution/SwerveSolution.h"
 
 namespace trajopt {
 
 expected<SwerveSolution, std::string> OptimalTrajectoryGenerator::Generate(
     const SwervePathBuilder& path, bool diagnostics, int64_t handle) {
-  SwerveDiscreteOptimal<_OPTI_BACKEND> problem(
+#if defined(OPTIMIZER_BACKEND_CASADI)
+  SwerveDiscreteOptimal<casadi::MX, CasADiOpti> problem(
+#elif defined(OPTIMIZER_BACKEND_SLEIPNIR)
+  SwerveDiscreteOptimal<SleipnirExpr, SleipnirOpti> problem(
+#endif
       path.GetPath(), path.GetControlIntervalCounts(),
       path.CalculateInitialGuess(), handle);
   return problem.Generate(diagnostics);
 }
+
 }  // namespace trajopt
