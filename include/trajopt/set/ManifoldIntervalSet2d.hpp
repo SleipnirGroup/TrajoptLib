ad70ce1365b31ca3982cfeacb912827df1afe865
shueja-personal <j.a.shue@outlook.com>
2024-06-21 20:44:15 -0700

    Added 2-arg and 1-arg constructor for ManifoldIntervalSet2d

--- include/trajopt/set/ManifoldIntervalSet2d.hpp
+++ include/trajopt/set/ManifoldIntervalSet2d.hpp
@@ -7,7 +7,7 @@
 namespace trajopt {
 
 /**
- * Manifold 2D set.
+ * Manifold 2D set for angles represented as cos-sin pairs.
  */
 struct TRAJOPT_DLLEXPORT ManifoldIntervalSet2d {
   /// The angle at the midpoint of the range.
@@ -15,6 +15,29 @@ struct TRAJOPT_DLLEXPORT ManifoldIntervalSet2d {
 
   /// Half of the width of the valid range.
   double tolerance;
+
+    /**
+   * Construct a manifold vector bound to a particular value +- a tolerance.
+   *
+   * @param middle The angle at the midpoint of the valid range
+   * @param tolerance The tolerance. Must be positive. Tolerances greater than pi,
+   * (half the period) do not constrain the vector at all.
+   */
+  constexpr ManifoldIntervalSet2d(double middle, double tolerance)
+      : middle(middle), tolerance(tolerance) {
+    assert(tolerance >= 0);
+  }
+
+  /**
+   * Construct a manifold vector bound that represents the given angle exactly.
+   *
+   * @param angle the angle of the vector to.
+   */
+  constexpr ManifoldIntervalSet2d(double angle)
+      : ManifoldIntervalSet2d(middle, 0) {
+  }
+
+  constexpr ManifoldIntervalSet2d() = default;
 };
 
 }  // namespace trajopt
